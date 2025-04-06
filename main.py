922a4e87a9402b12ae5c3bbe85866c663fcff2e3
Tomer Erez <tomererez1998@gmail.com>
2025-04-06 22:15:39 +0300

    add comments to training script

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import torch
 import os
-from core import build, iso, evaluation, train, save, parser,log_helpers
-import sys
+from core import iso
+from training_utils import evaluation, train, parser, save, log_helpers, build
 import evaluate as hf_evaluate
 import uuid
 
@@ -67,6 +67,20 @@ if __name__ == '__main__':
             train_wer=train_wer, eval_wer_clean=eval_wer_clean, eval_wer_perturbed=eval_wer_perturbed
         )
 
+
+
+        save.save_by_epoch(args=args, p=p, test_data_loader=test_data_loader,model=model,
+                           processor=processor, epoch_num=epoch)
+
+        save.save_loss_plot(train_scores=train_scores,eval_scores_perturbed=eval_scores_perturbed,eval_scores_clean=eval_scores_clean,
+                            save_dir=args.save_dir,norm_type=args.norm_type)
+
+        save.save_json_results(
+            save_dir=args.save_dir, norm_type=args.norm_type,attack_size=args.attack_size_string, epoch=epoch,
+            train_score={"ctc": train_ctc, "wer": train_wer},eval_score_clean={"ctc": eval_ctc_clean, "wer": eval_wer_clean},
+            eval_score_perturbed={"ctc": eval_ctc_perturbed, "wer": eval_wer_perturbed}
+        )
+
         #early stopping/save epoch and continue
         current_metric = eval_wer_perturbed if args.attack_mode == "targeted" else eval_ctc_perturbed
         if (args.attack_mode == "targeted" and current_metric < best_eval_score_perturbed) or \
@@ -75,40 +89,23 @@ if __name__ == '__main__':
             torch.save(p.detach().cpu(), os.path.join(args.save_dir, f"perturbation.pt"))
         else:
             no_improve_epochs += 1
-
         if no_improve_epochs > args.early_stopping:
             print(f'No improvements in {no_improve_epochs} epochs. Stopping early.')
             break
 
-        save.save_by_epoch(args=args, p=p, test_data_loader=test_data_loader,
-                           model=model, processor=processor, epoch_num=epoch)
-        save.save_loss_plot(train_scores=train_scores,
-                            eval_scores_perturbed=eval_scores_perturbed,
-                            eval_scores_clean=eval_scores_clean,
-                            save_dir=args.save_dir,
-                            norm_type=args.norm_type)
-
-        save.save_json_results(
-            save_dir=args.save_dir, norm_type=args.norm_type,
-            attack_size=args.attack_size_string, epoch=epoch,
-            train_score={"ctc": train_ctc, "wer": train_wer},
-            eval_score_clean={"ctc": eval_ctc_clean, "wer": eval_wer_clean},
-            eval_score_perturbed={"ctc": eval_ctc_perturbed, "wer": eval_wer_perturbed}
-        )
-
     # Final test evaluation
-    pert_ctc_test, pert_wer_test = evaluation.evaluate(
+    pert_ctc_test, pert_wer_test = evaluation.evaluate( #test the perturbation
         args=args, eval_data_loader=test_data_loader, p=p,
         model=model, wer_metric=wer_metric,
         perturbed=True, epoch_number=-1, processor=processor
     )
-    clean_ctc_test, clean_wer_test = evaluation.evaluate(
+    clean_ctc_test, clean_wer_test = evaluation.evaluate( #test on clean data
         args=args, eval_data_loader=test_data_loader, p=0,
         model=model, wer_metric=wer_metric,
         perturbed=False, epoch_number=-1, processor=processor
     )
 
-    save.save_loss_plot(
+    save.save_loss_plot( #create plots for the whole training process
         train_scores=train_scores,
         eval_scores_clean=eval_scores_clean,
         eval_scores_perturbed=eval_scores_perturbed,
@@ -125,7 +122,7 @@ if __name__ == '__main__':
         best_train_ctc = max(train_scores["ctc"])
         best_train_wer = max(train_scores["wer"])
 
-    save.save_json_results(
+    save.save_json_results( # save the result to a json file, easier to later parse and loop over all train logs
         save_dir=args.save_dir,
         norm_type=args.norm_type,
         attack_size=args.attack_size_string,
@@ -137,7 +134,7 @@ if __name__ == '__main__':
         final_test_perturbed={"ctc": pert_ctc_test, "wer": pert_wer_test}
     )
 
-    log_helpers.log_summary_metrics(
+    log_helpers.log_summary_metrics( # log the important stuff from the training process
     args=args,
     best_train_ctc=best_train_ctc,
     best_train_wer=best_train_wer,
